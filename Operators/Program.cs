4749401613092f0255a9414fe988546196159c03
Maysara <maysaracs1001@gmail.com>
2026-03-15 14:49:55 +0300

    Add Operator Project completed

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Threading.Tasks;
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arithemetic operators
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("a + b = " + (a + b));
+            Console.WriteLine("a - b = " + (a - b));
+            Console.WriteLine("a * b = " + (a * b));
+
+            // test operators
+            bool test = 5 < 6;
+            Console.WriteLine("5 < 6 = " + test);
+
+            // logical operators
+            bool test2 = 5> 6 && 7 < 9;
+            //bool test2 = 5 > 6 || 7 < 9;
+            Console.WriteLine("5 > 6 && 7 < 9 = " + test2);
+            Console.ReadKey();
+        }
+    }
+}
--- Operators/obj/Debug/net8.0/Operators.AssemblyInfo.cs
+++ Operators/obj/Debug/net8.0/Operators.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81981c4707d27334236a5ea41205afb49dcc22bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4998c623b97b94e8dbf09c4596c9b9faa5fe7d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
