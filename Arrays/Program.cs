4912602bb176c3eb195064793c4c7e765b393dc9
Maysara <maysaracs1001@gmail.com>
2026-03-15 15:15:04 +0300

    2D Arrays done

---
+++ Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] names = new string[2, 3];
+
+            names[0,0] = "John";
+            names[0,1] = "Jane";
+            names[0,2] = "Doe";
+
+            names[1,0] = "Alice";
+            names[1,1] = "Bob";
+            names[1,2] = "Charlie";
+
+            foreach (string n in names)
+            {
+                Console.WriteLine("Hello:" + n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
