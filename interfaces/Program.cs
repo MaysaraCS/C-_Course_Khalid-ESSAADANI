c9611c1cf86be70ae24d812ab14b070e1629f536
Maysara <maysaracs1001@gmail.com>
2026-03-16 17:04:13 +0300

    interfaces

---
+++ interfaces/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace interfaces
+{
+    interface Quadrangle
+    {
+        int width { get; set; }
+        int height { get; set; }
+        float Area();
+    }
+    class Rectangle : Quadrangle
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        int Quadrangle.width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+        int Quadrangle.height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+        float Quadrangle.Area()
+        {
+            return this.width * this.height;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrangle q = new Rectangle(5, 10);
+            Console.WriteLine("Width: " + q.width);
+            Console.WriteLine("Height: " + q.height);
+            Console.WriteLine("Area: " + q.Area());
+            Console.ReadKey();
+        }
+    }
+}
